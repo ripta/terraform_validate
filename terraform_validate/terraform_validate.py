1989ced283bc3ae60cb41cd88c103c5077c1896d
Ripta Pasay <ripta@users.noreply.github.com>
2019-01-17 21:43:01 -0800

    Remove deprecation decorator

--- terraform_validate/terraform_validate.py
+++ terraform_validate/terraform_validate.py
@@ -4,19 +4,6 @@ import re
 import warnings
 import json
 
-# def deprecated(func):
-#     '''This is a decorator which can be used to mark functions
-#     as deprecated. It will result in a warning being emitted
-#     when the function is used.'''
-#     def new_func(*args, **kwargs):
-#         print("The method '{0}' is deprecated as of v2.0. Please refer to the readme for the recommended usage of this library.".format(func.__name__))
-#         print("'{0}' will be removed in a future release.".format(func.__name__))
-#         return func(*args, **kwargs)
-#     new_func.__name__ = func.__name__
-#     new_func.__doc__ = func.__doc__
-#     new_func.__dict__.update(func.__dict__)
-#     return new_func
-
 class TerraformSyntaxException(Exception):
     pass
 
